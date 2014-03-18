541ccbb87514dd4b3eb75c11e728227ca2cbad0d
Brian Kenny <bkenny@gmailcom>
2014-03-17 20:39:27 -0400

    Revert "Delete admin.py"
    This reverts commit dc9c147031419b3f49f9531482cfb8cfeed538d6.

---
+++ datamanager/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin, messages
+from datamanager.models import GameList
+
+class GameListAdmin(admin.ModelAdmin):
+
+	def test_actions(GameListAdmin, request, queryset):
+		for g in queryset:
+			if g.processed == True:
+				messages.error(request, "%d has already been processed" % g.pk)
+				return
+		for g in queryset:
+			g.load_game_data()
+
+
+
+	actions_on_top = True
+	actions_on_bottom = True
+	list_display = ('__unicode__','processed',)
+	list_editable = ()
+
+	test_actions.short_description = "Load game to database"
+
+	actions = [test_actions,]
+
+
+admin.site.register(GameList, GameListAdmin)
